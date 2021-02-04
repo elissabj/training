272cd7bad9271da26f0a2b2cc0d29df6638279d8
elissabj <56558029+elissabj@users.noreply.github.com>
2021-02-04 16:50:20 -0600

    Add files via upload

---
+++ Omega Up/filtrando.cpp
@@ -0,0 +1,35 @@
+//https://omegaup.com/arena/problem/Filtrando-multiplos/
+#include<bits/stdc++.h>
+using namespace std;
+
+void ponerX(int n, int k, vector<int>arr){
+
+    string res = "";
+    for(int i = 0; i < n; i++){
+        if(arr[i] % k != 0){
+            res +="X";
+            res +=' ';
+        }else{
+            res += to_string(arr[i]);
+            res += ' ';
+        }
+    }
+
+    cout << res <<"\n";
+}
+
+
+int main(){
+
+    int n; cin >> n;
+    vector<int> numeros(n);
+
+    for(int i = 0; i < n; i++){
+        cin >> numeros[i];
+    }
+
+    int k; cin >> k;
+
+    ponerX(n,k,numeros);
+return 0;
+}
