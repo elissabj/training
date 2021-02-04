824685a5118c25f7a068d84537f67d24284f0e03
elissabj <56558029+elissabj@users.noreply.github.com>
2021-02-04 16:49:49 -0600

    Add files via upload

---
+++ Omega Up/prodPunto.cpp
@@ -0,0 +1,26 @@
+//https://omegaup.com/arena/problem/Producto-punto-de-dos-vectores/
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n; cin >> n; 
+    vector<int> a(n);
+    vector<int> b(n);
+
+    for(auto &valor: a){
+        cin >> valor;
+    }
+
+    for(auto &valor: b){
+        cin >> valor;
+    }
+
+    int suma_total = 0;
+    for(int i = 0; i < n; i++){
+        suma_total += a[i]*b[i];
+    }
+
+    cout << suma_total <<"\n";
+return 0;
+}
