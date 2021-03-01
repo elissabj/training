4871649552841926591ac5592e8aeefd93cba7b6
elissabj <56558029+elissabj@users.noreply.github.com>
2021-03-01 10:40:26 -0600

    Add files via upload

---
+++ Omega Up/aB.cpp
@@ -0,0 +1,18 @@
+//https://omegaup.com/arena/problem/A--B#problems
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+
+    int a,b; 
+    cin >> a >> b;
+
+    cout << a+b <<"\n";
+    cout << a-b <<"\n";
+    cout << a*b <<"\n";
+    cout << int(a/b) <<"\n";
+    cout << a%b <<"\n";
+
+return 0;
+}
