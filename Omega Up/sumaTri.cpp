9b6c8584c380cc96f65f16e58ca35e0321513acc
elissabj <56558029+elissabj@users.noreply.github.com>
2021-03-03 02:03:39 -0600

    Add files via upload

---
+++ Omega Up/sumaTri.cpp
@@ -0,0 +1,63 @@
+//https://omegaup.com/arena/problem/Suma-Acarreada/#problems
+#include<bits/stdc++.h>
+using namespace std;
+
+void sumaCadenas(string &x, string &y){
+
+    int acarreo = 0;
+    string suma = "";
+
+    if(x.size() > y.size()) swap(x,y);
+    reverse(x.begin(), x.end());
+    reverse(y.begin(), y.end());
+
+    for(int i = 0; i < x.size(); i++){
+        int sum_actual = (x[i]-'0') + (y[i]-'0') + acarreo;
+        suma += (sum_actual%10)+'0';
+        acarreo = sum_actual/10;
+    }
+
+    for(int i = x.size(); i < y.size(); i++){
+        int sum_actual = (y[i]-'0') + acarreo;
+        suma += (sum_actual%10)+'0';
+        acarreo = sum_actual/10;
+    }
+
+    if(acarreo) suma += acarreo+'0';
+
+    for(int i = suma.size()-1; i >= 0; i--){
+        cout << suma[i];
+    }
+    cout<<"\n";
+    return;
+}
+
+int main(){
+
+    int n, aux; 
+    
+    cin >> n;
+    string x, y;
+    bool flag = false;
+    for(int i = 0; i < n; i++){
+        cin >>aux;
+        if(aux == 0){
+            if(x.size()!= 0){
+                 x += aux+'0';
+            }
+        }else x += aux+'0';
+    }
+
+    for(int i = 0; i < n; i++){
+        cin >>aux;
+        if(aux == 0){
+            if(y.size()!= 0){
+                 y += aux+'0';
+            }
+        }else y += aux+'0';
+    }
+
+    sumaCadenas(x,y);
+
+return 0;
+}
