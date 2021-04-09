8ea78b0bf2a5bf5416d708d761179b57f9b7e493
elissabj <56558029+elissabj@users.noreply.github.com>
2021-04-09 02:45:31 -0500

    Add files via upload

--- Codeforces/chatRoom.cpp
+++ Codeforces/chatRoom.cpp
@@ -1,25 +1,23 @@
 //https://codeforces.com/problemset/problem/58/A
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-
-     string s; cin >> s;
-
-    bool yaEncontre = false;
-    string check = "hello";
-    int cont = 0;
+int main (){
+    
+    string s;
+    cin>>s;
+    
+    string helo = "hello";
+    int idx = 0;
+    int ban = 0; 
     for(int i = 0; i < s.size(); i++){
-        if(s[i] == check[cont]){
-            if (s[i] == 'o'){ yaEncontre = true; break;}
-            cont++;
+        if(s[i] == helo[idx]){
+            idx++;
+            if(idx == 5){ban = 1; break;}
         }
     }
-
-    if(yaEncontre){
-        cout <<"YES\n";
-    }else{
-        cout <<"NO\n";
-    }
-return 0;
+    
+    if(ban)cout<<"YES\n"; else cout<<"NO\n";
+    
+    return 0;
 }
