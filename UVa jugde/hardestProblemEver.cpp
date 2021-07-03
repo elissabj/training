f66cc604b2bb9a0e5b9cfa7286e4a7a037636242
elissabj <56558029+elissabj@users.noreply.github.com>
2021-07-03 15:18:13 -0500

    UVA 12478 - Hardest Problem Ever (Easy)

---
+++ UVa jugde/hardestProblemEver.cpp
@@ -0,0 +1,8 @@
+//https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=609&page=show_problem&problem=3922
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout<<"KABIR\n";
+return 0;
+}
