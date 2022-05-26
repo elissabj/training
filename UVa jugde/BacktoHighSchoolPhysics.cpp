2cfe146e0e9f6ab6aaac7b3f6d41be6f5dd9340a
elissabj <56558029+elissabj@users.noreply.github.com>
2022-05-26 00:04:59 -0500

    UVA 10071 - Back to High School Physics

---
+++ UVa jugde/BacktoHighSchoolPhysics.cpp
@@ -0,0 +1,17 @@
+//https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=1012
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+
+   int t, v; 
+   
+   while(cin >> t && cin >> v){
+       cout << t*2*v <<"\n";
+   }
+
+
+return 0;
+}
